dcfd115224e0037524eb31c5e10e076eb200df39
JB <jb@boyce.com.au>
2018-04-11 14:50:27 +1000

    Renamed referred components. Changed footer.

--- Vue.Starter/src/layout/app/app.ts
+++ Vue.Starter/src/layout/app/app.ts
@@ -3,10 +3,10 @@ import { Component } from "vue-property-decorator";
 
 @Component({
     components: {
-		HeaderLayout: require("../header/header.vue.html").default,
-		SideMenuLayout: require("../menu/side.vue.html").default,
-		AsideMenuLayout: require("../menu/aside.vue.html").default,
-		FooterLayout: require("../footer/footer.vue.html").default,
+		AppHeader: require("../header/header.vue.html").default,
+		SideMenu: require("../menu/side.vue.html").default,
+		AsideMenu: require("../menu/aside.vue.html").default,
+		AppFooter: require("../footer/footer.vue.html").default,
     }
 })
 export default class AppComponent extends Vue {
